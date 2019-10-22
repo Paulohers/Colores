4df667ac014214bef0d49ae876f55682e6844cf9
Paulohers <rl_1998@hotmail.com>
2019-10-21 19:42:45 -0700

    V1

--- app/src/main/java/com/example/colores/MainActivity.java
+++ app/src/main/java/com/example/colores/MainActivity.java
@@ -18,93 +18,133 @@ public class MainActivity extends AppCompatActivity {
     * @Autor=Paulo
      * @param button: Este párametro se utiliza para inicializar el botón y poder instanciarlo con su respectivo id
      * @param spinner: Este párametro se utiliza para inicializar el spinner y poder instanciarlo con su respectivo id.
+     * @param C1,C2,C3: Estos párametros estan siendo declarados para mas adelante ser utilizados con el id de un botón en el diseño de la aplicación.
      * En este apartado se declaran variables de tipo Button y Spinner para
      * su posterior utilización.
     * */
     Button button;
     Spinner spinner;
-    Spinner spinner2;
+    Button C1,C2,C3;
 
-    /**
-     * Método principal donde se mandan llamar los métodos.
-     *
-     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        /**
+         * Método principal donde se mandan llamar los métodos.
+         *
+         */
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
          InicializarV();
 
          ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.opciones, android.R.layout.simple_spinner_item);
          spinner.setAdapter(adapter);
-         spinner2.setAdapter(adapter);
         ColoresM();
 
     }
-    /**
-    * Método en el cual se inicializan las variables del layout como los spinners y buttons
-    * */
+
     public void InicializarV(){
+        /**
+         * Método en el cual se inicializan las variables del layout como los spinners y buttons
+         * @param C1: es un boton el cual cambiara de color, estoy consiguiendo el id que contiene en el xml de diseño para poder utilizarlo, y declararlo
+         * en una variable tipo Button.
+         * @param C2: es un boton el cual cambiara de color, estoy consiguiendo el id que contiene en el xml de diseño para poder utilizarlo, y declararlo
+         * en una variable tipo Button.
+         * @param C3: es un boton el cual cambiara de color, estoy consiguiendo el id que contiene en el xml de diseño para poder utilizarlo, y declararlo
+         * en una variable tipo Button.
+         * */
         spinner = (Spinner) findViewById(R.id.spinner);;
-        spinner2 = (Spinner) findViewById(R.id.spinner2);;
         button = (Button) findViewById(R.id.button);;
+        C1 = findViewById(R.id.C1);
+        C2 = findViewById(R.id.C2);
+        C3 = findViewById(R.id.C3);
     }
 
-    /**
-     * Método en el cual se toman desiciones mediante if para determinar que opcion esta ingresada en
-     * el  control tipo Spinner.
-     * @param: sp1, donde se guardara la opción ingresada en el spinner y se comparara con el otro spinner2.
-     * @param: sp2, donde se guardara la opción ingresada en el spinner y se comparara con el otro spinner.
-     */
+
     public void ColoresM(){
+        /**
+         * Método en el cual se toman desiciones mediante un switch para determinar que opcion estara ingresada en
+         * el  control tipo Spinner.
+         * @param sp1: , variable tipo string,donde se guardara la opción ingresada en el spinner y se comparara dentro del switch con el contenido que lleva dentro,
+         * en este caso un string
+         */
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 String sp1 = spinner.getSelectedItem().toString();
-                String sp2 = spinner2.getSelectedItem().toString();
+                switch (sp1){
+                    case "Azul":
+                        C3.setVisibility(View.VISIBLE);
+                        C1.setBackgroundColor(Color.BLUE);
+                        C2.setBackgroundColor(Color.BLUE);
+                        C3.setBackgroundColor(Color.BLUE);
+                        button.setTextColor(Color.BLACK);
+                        button.setBackgroundColor(Color.BLUE);
+                        break;
+                    case "Rojo":
+                        C3.setVisibility(View.VISIBLE);
+                        C1.setBackgroundColor(Color.RED);
+                        C2.setBackgroundColor(Color.RED);
+                        C3.setBackgroundColor(Color.RED);
+                        button.setTextColor(Color.BLACK);
+                        button.setBackgroundColor(Color.RED);
+                        break;
+                    case "Verde":
+                        C1.setBackgroundColor(Color.YELLOW);
+                        C2.setBackgroundColor(Color.BLUE);
+                        C3.setVisibility(View.INVISIBLE);
+                        button.setTextColor(Color.BLACK);
+                        button.setBackgroundColor(Color.GREEN);
+                        break;
+                    case "Amarillo":
+                        C3.setVisibility(View.VISIBLE);
+                        C1.setBackgroundColor(Color.YELLOW);
+                        C2.setBackgroundColor(Color.YELLOW);
+                        C3.setBackgroundColor(Color.YELLOW);
+                        button.setTextColor(Color.BLACK);
+                        button.setBackgroundColor(Color.YELLOW);
+                        break;
+                    case "Naranja":
+                        C1.setBackgroundColor(Color.RED);
+                        C2.setBackgroundColor(Color.YELLOW);
+                        C3.setVisibility(View.INVISIBLE);
+                        button.setTextColor(Color.BLACK);
+                        button.setBackgroundColor(Color.rgb(255,255,0));
+                        break;
+                    case "Magenta":
+                        C1.setBackgroundColor(Color.RED);
+                        C2.setBackgroundColor(Color.BLUE);
+                        C3.setVisibility(View.INVISIBLE);
+                        button.setTextColor(Color.BLACK);
+                        button.setBackgroundColor(Color.rgb(255,0,255));
+                        break;
+                    case "Cian":
+                        C1.setBackgroundColor(Color.BLUE);
+                        C2.setBackgroundColor(Color.GREEN);
+                        C3.setVisibility(View.INVISIBLE);
+                        button.setTextColor(Color.BLACK);
+                        button.setBackgroundColor(Color.CYAN);
+                        break;
+                    case "Negro":
+                        C3.setVisibility(View.VISIBLE);
+                        C1.setBackgroundColor(Color.BLUE);
+                        C2.setBackgroundColor(Color.RED);
+                        C3.setBackgroundColor(Color.GREEN);
+                        button.setBackgroundColor(Color.BLACK);
+                        button.setTextColor(Color.WHITE);
+                        break;
+                    case "Blanco":
+                        C3.setVisibility(View.VISIBLE);
+                        C1.setBackgroundColor(Color.CYAN);
+                        C2.setBackgroundColor(Color.rgb(255,0,255));
+                        C3.setBackgroundColor(Color.GREEN);
+                        button.setTextColor(Color.BLACK);
+                        button.setBackgroundColor(Color.WHITE);
+                        break;
 
-                if ((sp1.equals("Azul") && sp2.equals("Amarillo")) || (sp1.equals("Amarillo") && sp2.equals("Azul"))){
-                    button.setBackgroundColor(Color.GREEN);
-                    button.setText("VERDE");
-                }else{
-                    if ((sp1.equals("Amarillo") && sp2.equals("Rojo")) || (sp1.equals("Rojo") && sp2.equals("Amarillo"))){
-                        int a = Color.rgb(255, 165, 0);
-                        button.setBackgroundColor(a);
-                        button.setText("NARANJA");
-                    }else{
-                        if ((sp1.equals("Azul") && sp2.equals("Rojo")) || (sp1.equals("Rojo") && sp2.equals("Azul"))){
-
-                            int a = Color.rgb(167, 4, 254);
-                            button.setBackgroundColor(a);
-                            button.setText("Magenta");
-                        }else {
-                            if ((sp1.equals("Azul") && sp2.equals("Verde")) || (sp1.equals("Verde") && sp2.equals("Azul"))){
-
-                                int a = Color.rgb(167, 4, 254);
-                                button.setBackgroundColor(a);
-                                button.setText("Cian");
-                            }else {
-                                if ((sp1.equals("Amarillo") && sp2.equals("Cian")) || (sp1.equals("Cian") && sp2.equals("Amarillo"))){
-                                    button.setBackgroundColor(Color.GREEN);
-                                    button.setText("VERDE");
-                                }else{
-                                    if ((sp1.equals("Amarillo") && sp2.equals("Magenta")) || (sp1.equals("Magenta") && sp2.equals("Amarillo"))){
-                                        button.setBackgroundColor(Color.RED);
-                                        button.setText("Rojo");
-                                    }else {
-                                        if ((sp1.equals("Cian") && sp2.equals("Magenta")) || (sp1.equals("Magenta") && sp2.equals("Cian"))){
-                                            button.setBackgroundColor(Color.GREEN);
-                                            button.setText("VERDE");
-                                        }else {
-                                            button.setBackgroundColor(Color.GRAY);
-                                            button.setText("What ?");
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
                 }
+
+
             }
         });
     }
